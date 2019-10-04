3e1c55e7d12c08d1e0ca1df7179694ec6ef71d95
FionaDL <fionadlapham@gmail.com>
2019-10-04 13:18:07 -0400

    adding set_current_user

--- bird-banding-api/app/controllers/api/v1/sessions_controller.rb
+++ bird-banding-api/app/controllers/api/v1/sessions_controller.rb
@@ -1,14 +1,20 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-    @user = User.find_by(username: params[:session][:username])
-
-    if @user && @user.authenticate(params[:session][:password])
+    @user = User.find_by(username: params[:credentials][:username])
+    if @user && @user.authenticate(params[:credentials][:password])
       session[:user_id] = @user.id
       render json: @user
     else
-      render json: {error: "Ivalid Username or Password."}
+      render json: {error: "Invalid Username or Password."}
     end
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: current_user
+    else
+      render json: {error: "No one is logged in."}
   end
 
   def destroy
