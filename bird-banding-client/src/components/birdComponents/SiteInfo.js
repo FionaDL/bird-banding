aa4e6935a8c9d3a5e2165ff123bcbd8e2792b678
FionaDL <fionadlapham@gmail.com>
2019-11-06 10:44:43 -0500

    fix typo in site-info

--- bird-banding-client/src/components/birdComponents/SiteInfo.js
+++ bird-banding-client/src/components/birdComponents/SiteInfo.js
@@ -8,7 +8,7 @@ const SiteInfo = () => {
 
   return(
   <div className="card">
-    Bird banding or bird ringing is an international technique used for studying and identifying individual birds, as well as migration patterns. Banding stations collect data on indivdual birds including information such as weight, sex and wingspan. The practice of butting a ring on the birds leg with an indidual banding number allows scientists to follow a bird throughout its life. Log In or Sign Up to upload data instantly and share data between banding stations!
+    Bird banding or bird ringing is an international technique used for studying and identifying individual birds, as well as migration patterns. Banding stations collect data on indivdual birds including information such as weight, sex and wingspan. The practice of putting a ring on the birds leg with an indidual banding number allows scientists to follow a bird throughout its life. Log In or Sign Up to upload data instantly and share data between banding stations!
   </div>
 )
 }
